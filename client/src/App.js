cceaedf553dac8155cf1f3d07e9b01c7af1c69be
PabloLandro <73550306+PabloLandro@users.noreply.github.com>
2023-01-14 11:57:56 +0100

    change colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import './styles/App.css'
+import './styles/global.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Hello world!</h1>
       </header>
+      <body className="App-body">
+
+      </body>
     </div>
   );
 }
